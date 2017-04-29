49d92ff2000ad5c3428c4182420a9d8d4327196b
Alexander Dejeu <alexdejeu@gmail.com>
2017-04-29 12:25:16 -0700

    Decided to wipe the original code I had from before and rewrite.  Added noice str rep function

--- two_three_tree.py
+++ two_three_tree.py
@@ -1,92 +1,72 @@
 class Node(object):
     def __init__(self, data, children=None, parent_node=None):
-        self.data = list(data)
-        self.children = list(children)
+        if type(data) is list:
+            self.data = data
+        else:
+            data_list = [data]
+            self.data = data_list
+
+        if children is not None:
+            self.children = list(children)
+        else:
+            self.children = []
         self.parent_node = parent_node
-
-    def get_next_path(self, value):
-        '''return Node where value fits'''
-        if value <= self.data[0]:
-            return self.children[0]
-        elif value > self.data[0] and value < self.data[1]:
-            return self.children[1]
-        else:
-            return self.children[2]
-
-    def add(self, value):
-        if self.data == 1:
-            self.data = sorted(self.data.append(value))
-        elif self.data == 0:
-            self.data.append(value)
-        else:
-            # Have to deal with that silly silly spliting
-            # Ok so lets add the elemnt, sort and find the middle value
-            self.data = sorted(self.data.append(value))
-            data_to_push_up = self.data[1]
-
-            # We are on the root...
-            if self.parent_node is None:
-                new_root = Node(data_to_push_up)
-                self.root = new_root
-            self.parent_node.add(data_to_push_up)
 
 
 class TwoThreeTree(object):
     def __init__(self):
         self.root = None
 
-    def add_node(self, node, value):
-        if len(node.data) == 1:
-            node.data = sorted(self.data.append(value))
+    def __str__(self):
+        self.inorder_print(self.root)
+        return "representation"
 
-            # SET NEW CHILDREN
-        elif len(node.data) == 0:
-            node.data.append(value)
+    def inorder_print(self, node):
+        child_count = len(node.children)
+        if child_count == 0:
+            for value in node.data:
+                print(value)
         else:
-            # Have to deal with that silly silly spliting
-            # Ok so lets add the elemnt, sort and find the middle value
-            node.data = sorted(node.data.append(value))
-            data_to_push_up = self.data[1]
+            for index in xrange(child_count - 1):
+                # There is one more child than data so we need to skip
+                if index == child_count - 1:
+                    self.inorder_print(node.children[index])
+                else:
+                    self.inorder_print(node.children[index])
+                    print(node.data[index])
 
-            # We are on the root...
-            if node == self.root:
-                new_root = Node(data_to_push_up)
-                new_left_node = Node(node.data[0])
-                new_right_node = Node(node.data[1])
-                new_left_node.parent_node = new_root
-                new_right_node.parent_node = new_root
-
-                new_root.children = list(new_left_node, new_right_node)
-
-                self.root = new_root
-            else:
-                self.add_node(node.parent_node, data_to_push_up)
+    def add_node(self, node, value):
+        pass
 
     def insert(self, value):
-        '''Lmao going to logic it up
-         1 - check empty state
-         2 - check single node state
-         3 - check everything else... '''
-        current_node = self.root
-        if current_node is None:
+        ''' 1. If the tree is empty, create a node and put value into the node
+            2. Otherwise find the leaf node where the value belongs.
+            3. If the leaf node has only one value, put the new value into the
+                node
+            4. If the leaf node has more than two values, split the node and
+                promote the median of the three values to parent.
+            5. If the parent then has three values, continue to split and
+                promote, forming a new root node if necessary
+        '''
+        # 1. If the tree is empty
+        if self.root is None:
             self.root = Node(value)
-            return True
 
-        # Find the leaf node
+        # 2. Find leaf where value belongs
 
-        # IF ROOT DOES HAVE CHILDREN YOU GO TO leaf
-        # OTHERWISE ADD TO ROOT
-        while current_node.children is not None:
-            current_node = current_node.get_next_path(value)
+        # 3. If leaf only has one value put the new value there!
 
-        self.add_node(current_node, value)
+        # 4. If leaf has more than 2 elements. Split and promote median to parent
+
+        # 5. Repeat on the parent - worse case form a new root.
+        pass
 
     def search(self, value):
-        current_node = self.root
+        pass
 
-        while current_node is not None:
-            if value in current_node.data:
-                return True
-            if current_node.children is None:
-                return False
-            current_node = current_node.get_next_path(value)
+
+
+
+test_tree = TwoThreeTree()
+test_tree.insert(4)
+print(test_tree)
